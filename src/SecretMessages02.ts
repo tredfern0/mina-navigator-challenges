148abf99be310acbd9f61e0afaab3157d15e07e2
Thomas <thomasr928@gmail.com>
2024-02-23 14:22:10 -0700

    Add basic logic for reducing messages

--- src/SecretMessages02.ts
+++ src/SecretMessages02.ts
@@ -1,22 +1,54 @@
 import { Field, Reducer, Struct, SmartContract, state, State, method, Bool, Provable, UInt64 } from 'o1js';
 
+// SecretMessage will be the input to the contract. In order to satisfy this condition:
+// "The message details should be private inputs."
+// We will verify the validity of the message, but then ONLY save the messageNumber
+// for the reducer to process.
+export class SecretMessage extends Struct({ messageNumber: UInt64, agentId: Field, agentXLocation: Field, agentYLocation: Field, checkSum: Field }) { }
 
-export class SecretMessage extends Struct({ messageNumber: UInt64, agentId: Field, agentXLocation: Field, agentYLocation: Field, checkSum: Field }) {
-    // Can we move the other functions into here?
-    static isValid(message: SecretMessage): Bool {
-        return Bool(true);
-    }
-}
+// This will be the format of the message saved for the reducer to process
+export class PublicMessage extends Struct({ isValid: Bool, messageNumber: UInt64 }) { }
 
+
+// 2. This program is needed to run on low spec hardware so you need to find a way to process the batch so that the circuit size remains low.
 
 export class BatchMessages extends SmartContract {
     // Highest message number received...
     @state(UInt64) messageNumber = State<UInt64>();
-    reducer = Reducer({ actionType: SecretMessage });
+    // Store the action state so we can efficiently filter out the actions that have already been processed
+    @state(Field) actionState = State<Field>();
+
+    // reducer = Reducer({ actionType: SecretMessage });
+    reducer = Reducer({ actionType: PublicMessage });
 
     init() {
         super.init();
         this.messageNumber.set(UInt64.from(0));
+    }
+
+    @method dispatchIfValid(message: SecretMessage, prevMessageNumber: UInt64) {
+        const validBool = this.isValid(message, prevMessageNumber);
+        this.reducer.dispatch({ isValid: validBool, messageNumber: message.messageNumber });
+    }
+
+    isValid(message: SecretMessage, prevMessageNumber: UInt64): Bool {
+        // In case the message number is not greater than the previous one, 
+        // this means that this is a duplicate message.  
+        // In this case it still should be processed but the message details 
+        // do not need to be checked.
+        const messageNew = message.messageNumber.greaterThan(prevMessageNumber);
+        // If Agent ID is zero we don't need to check the other values, but this is still a valid message
+        const agent0 = message.agentId.equals(0);
+
+        const check1 = this.checksumCheck(message.agentId, message.agentXLocation, message.agentYLocation, message.checkSum);
+        const check2 = this.rangeCheck(message.agentId, message.agentXLocation, message.agentYLocation);
+        const checksOk: Bool = check1.and(check2);
+
+        // agent0 or messageNew = automatic approval
+        // If both of those are false, then checksOk must be true
+        // So condition is: messageNew OR agent0 OR checksOk
+        const wasValid = agent0.or(messageNew).or(checksOk);
+        return wasValid;
     }
 
     rangeCheck(agentId: Field, agentXLocation: Field, agentYLocation: Field): Bool {
@@ -39,16 +71,43 @@ export class BatchMessages extends SmartContract {
         return checkSum.equals(checkSumExpected);
     }
 
+    @method runReduce(splitBatch: Bool) {
 
-    @method validateMessage(messageNumber: UInt64, agentId: Field, agentXLocation: Field, agentYLocation: Field, checkSum: Field) {
-        const check1 = this.checksumCheck(agentId, agentXLocation, agentYLocation, checkSum);
-        const check2 = this.rangeCheck(agentId, agentXLocation, agentYLocation);
-        const checksOk: Bool = check1.and(check2);
+        const actionState = this.actionState.getAndRequireEquals();
 
-        // If Agent ID is zero we don't need to check the other values, but this is still a valid message
-        agentId.equals(0).or(checksOk).assertTrue("Invalid message");
+        // Interpreting instructions to mean that we find/process highest
+        // message number in each batch - even if it's lower than the previously
+        // committed message number!
+        // So setting initial state to be 0, lowest possible message number
+        // and then processing all messages in the batch
+        // So we will not event get messageNumber - we'll just overwrite it
+        // const messageNumber = this.messageNumber.getAndRequireEquals();
 
-        this.messageNumber.set(messageNumber);
+        const pendingActions = this.reducer.getActions({
+            fromActionState: actionState,
+        });
+
+        const initial = {
+            state: UInt64.from(0),
+            // TODO - understand what exactly this is...
+            actionState: Reducer.initialActionState,
+        }
+
+        let { state: newMessage, actionState: newActionState } = this.reducer.reduce(
+            pendingActions,
+            // state type
+            UInt64,
+            // function that says how to apply an action
+            (state: UInt64, action: PublicMessage) => {
+                // So if our action 'isValid' bool is true, then take max...
+                const maxValue = Provable.if(state.greaterThan(action.messageNumber), state, action.messageNumber);
+                const retValue = Provable.if(action.isValid, maxValue, state);
+                return retValue;
+            },
+            initial
+        );
+
+        this.messageNumber.set(newMessage.messageNumber);
+        this.actionState.set(newActionState);
     }
-
 }
